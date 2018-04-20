d74be37ac05e560fe5d105842d7eb062feeb501b
【ユーザー名】gulp <nakamura@kamosu-ya.jp>
2018-04-21 01:39:33 +0900

    menu形になってきた

--- belise-lite-child/menu-page.php
+++ belise-lite-child/menu-page.php
@@ -1,11 +1,8 @@
 <?php
 /**
- * Template Name: page-menu
+ * Template Name: menu-page
  *
- * This is the template that displays all pages by default.
- * Please note that this is the WordPress construct of pages
- * and that other 'pages' on your WordPress site may use a
- * different template.
+ * Template that displays pages full width with big header section.
  *
  * @link https://codex.wordpress.org/Template_Hierarchy
  *
@@ -13,16 +10,26 @@
  */
 
 get_header(); ?>
-
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main">
-			<div class="container single-container">
+
+
 
 				<?php
 				while ( have_posts() ) :
 					the_post();
+				    ?>
 
-					get_template_part( 'template-parts/content', 'page' );
+                <div class="eye-img" style="background-image: url(<?php echo wp_get_attachment_url( get_post_thumbnail_id() ); ?>">
+
+
+
+
+                </div>
+                    <h1><?php echo get_the_title(); ?></h1>
+                <?php
+
+					get_template_part( 'template-parts/content', 'page-full-width' );
 
 					// If comments are open or we have at least one comment, load up the comment template.
 					if ( comments_open() || get_comments_number() ) :
@@ -32,16 +39,16 @@ get_header(); ?>
 				endwhile; // End of the loop.
 				?>
 
-			</div>
-
             <div id="access-footer">
                 <?php // access-footer挿入
                 $my_post = get_page_by_title ( 'access-footer' );
                 echo apply_filters ( 'the_content', $my_post -> post_content );
                 ?>
             </div>
+
 		</main><!-- #main -->
 	</div><!-- #primary -->
 
 <?php
 get_footer();
+
